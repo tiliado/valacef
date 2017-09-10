ca9b30fbe67e43e36f5a0772494b209525df18e2
Jiří Janoušek <janousek.jiri@gmail.com>
2017-09-10 20:01:15 +0200

    Add string functions
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- overrides/cef_string.h
+++ overrides/cef_string.h
@@ -1,53 +1,51 @@
-/* template <class T>
-class CefStringBase {
- public:
-
-  CefStringBase() : CefStringBase(NULL);
-  CefStringBase(const CefStringBase& str);
-  CefStringBase(const std::string& src);
-  CefStringBase(const char* src);
-  CefStringBase(const char* src, size_t src_len, bool copy);
-
-  const char* c_str();
-  size_t length() const;
-  bool empty() const;
-  int compare(const CefStringBase& str);
-  void clear();
-  bool IsOwner() const;
-  void ClearAndFree();
-  bool FromString(const char* src, size_t src_len, bool copy);
-  bool FromASCII(const char* str);
-  std::string ToString() const;
-  bool FromString(const std::string& str);
-
-  bool operator<(const CefStringBase& str) const;
-  bool operator<=(const CefStringBase& str) const;
-  bool operator>(const CefStringBase& str) const;
-  bool operator>=(const CefStringBase& str) const;
-  bool operator==(const CefStringBase& str) const;
-  bool operator!=(const CefStringBase& str) const;
-
-  CefStringBase& operator=(const CefStringBase& str);
-  operator std::string() const;
-//  CefStringBase& operator=(std::string& str);
-  CefStringBase& operator=(const char* str);
-};
-
-struct CefStringTraitsUTF8 {
-
-};
-*/
-
 typedef struct _cef_string_utf8_t {
   char* str;
   size_t length;
   void (*dtor)(char* str);
 } cef_string_utf8_t;
 
-//typedef short unsigned int char16
-
 typedef struct _cef_string_utf16_t {
   char16* str;
   size_t length;
   void (*dtor)(char16* str);
 } cef_string_utf16_t;
+
+
+///
+// These functions convert between UTF-8, -16, and -32 strings. They are
+// potentially slow so unnecessary conversions should be avoided. The best
+// possible result will always be written to |output| with the boolean return
+// value indicating whether the conversion is 100% valid.
+///
+int cef_string_utf8_to_utf16(const char* src, size_t src_len, cef_string_t* output);
+
+///
+// These functions convert between UTF-8, -16, and -32 strings. They are
+// potentially slow so unnecessary conversions should be avoided. The best
+// possible result will always be written to |output| with the boolean return
+// value indicating whether the conversion is 100% valid.
+///
+int cef_string_utf16_to_utf8(const char16* src, size_t src_len, cef_string_utf8_t* output);
+
+///
+// These functions set string values. If |copy| is true (1) the value will be
+// copied instead of referenced. It is up to the user to properly manage
+// the lifespan of references.
+///
+int cef_string_utf16_set(const char16* src, size_t src_len, cef_string_utf16_t* output, int copy);
+
+///
+// Convenience macros for copying values.
+///
+int cef_string_utf16_copy(const char16* src, size_t src_len, cef_string_utf16_t* output);
+
+///
+// These functions clear string values. The structure itself is not freed.
+///
+void cef_string_utf16_clear(cef_string_utf16_t* str);
+
+///
+// These functions free the string structure allocated by the associated
+// alloc function. Any string contents will first be cleared.
+///
+void cef_string_userfree_utf16_free(cef_string_userfree_utf16_t str);
